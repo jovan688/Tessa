a4dbd048a07eb3b4a1662d51d6547d030c36398c
José Rostrán Cerna <jovan688@gmail.com>
2019-11-24 00:30:35 -0600

    Agregando el cambio

---
+++ src/main/java/com/globalmedia360/Tessa/TessaApplication.java
@@ -0,0 +1,13 @@
+package com.globalmedia360.Tessa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TessaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TessaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globalmedia360/Tessa/TessaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalmedia360.Tessa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TessaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
